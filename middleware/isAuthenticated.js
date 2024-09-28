8f268b188c2914416ac7cacaf75db3bef0af3587
Ashwin Sharma <sharmaashwin203@gmail.com>
2024-09-29 02:37:36 +0530

    authorization bearer

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,14 +1,30 @@
 import jwt from "jsonwebtoken";
+
 const isAuthenticated = async (req, res, next) => {
   try {
-    const cookieString = req?.headers.cookie;
-    const cookies = cookieString.split("; ").reduce((acc, cookie) => {
-      const [key, value] = cookie.split("=");
-      acc[key] = value;
-      return acc;
-    }, {});
-    const token = cookies.authToken;
-    // console.log("Token", token);
+    // Try to get the token from the Authorization header
+    let token = req.headers.authorization;
+
+    // If token is found in Authorization header, remove the "Bearer " prefix
+    if (token) {
+      token = token.replace("Bearer ", "");
+    }
+
+    // If token is not found in Authorization header, try to get it from the cookie header
+    if (!token) {
+      const cookieString = req.headers.cookie;
+
+      if (cookieString) {
+        const cookies = cookieString.split("; ").reduce((acc, cookie) => {
+          const [key, value] = cookie.split("=");
+          acc[key] = value;
+          return acc;
+        }, {});
+
+        token = cookies.authToken;
+      }
+    }
+    // console.log(token);
     if (!token) {
       return res.status(401).json({
         message: "User is not authenticated.",
@@ -34,4 +50,5 @@ const isAuthenticated = async (req, res, next) => {
     });
   }
 };
+
 export default isAuthenticated;
